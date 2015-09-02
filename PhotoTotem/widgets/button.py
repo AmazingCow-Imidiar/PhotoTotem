32c39a6121e0a8e59eecfbaff23c18c46d5973cc
N2OMatt <n2o.matt@gmail.com>
2015-09-02 17:27:20 -0300

    Implement the mouse event handlers. Add the click callback. Add some COWTODOs.

--- PhotoTotem/widgets/button.py
+++ PhotoTotem/widgets/button.py
@@ -27,7 +27,6 @@ import pygame.locals;
 from base_widget import BaseWidget;
 from sprite      import Sprite;
 
-
 class Button(BaseWidget):
     ############################################################################
     ## CTOR                                                                   ##
@@ -42,19 +41,26 @@ class Button(BaseWidget):
         self.__pressed_sprite = Sprite();
         self.__current_sprite = None;
 
+        self.__click_callback = None;
+
 
     ############################################################################
     ## Set Image Methods                                                      ##
     ############################################################################
     def set_sprite_filenames(self, normal_filename, pressed_filename):
+        #Assign the image filenames for both sprites.
         self.set_normal_sprite_filename(normal_filename);
         self.set_pressed_sprite_filename(pressed_filename);
 
     def set_normal_sprite_filename(self, normal_filename):
+        #Assign the image filename to normal state and reset
+        #the button to normal state.
         self.__normal_sprite.set_image_filename(normal_filename);
         self.reset();
 
     def set_pressed_sprite_filename(self, pressed_filename):
+        #Assign the image filename to pressed state and reset
+        #the button to normal state.
         self.__pressed_sprite.set_image_filename(pressed_filename);
         self.reset();
 
@@ -62,13 +68,27 @@ class Button(BaseWidget):
     ## Position Methods                                                       ##
     ############################################################################
     def set_position(self, x, y):
+        #Set button's position.
         self.x = x;
         self.y = y;
 
+        #Set the state sprite positions.
         self.__normal_sprite.set_position(x, y);
         self.__normal_sprite.set_position(x, y);
+        self.__pressed_sprite.set_position(x, y);
+        self.__pressed_sprite.set_position(x, y);
 
+        #Reset button state to normal state.
         self.reset();
+
+    def get_bounding_box(self):
+        return self.__current_sprite.get_bounding_box();
+
+    ############################################################################
+    ## Callback Methods                                                       ##
+    ############################################################################
+    def set_click_callback(self, callback):
+        self.__click_callback = callback;
 
     ############################################################################
     ## State Methods                                                          ##
@@ -76,17 +96,20 @@ class Button(BaseWidget):
     def reset(self):
         self.__set_normal_state();
 
+    def __set_pressed_state(self):
+        self.__current_sprite = self.__pressed_sprite;
+
+    def __set_normal_state(self):
+        self.__current_sprite = self.__normal_sprite;
 
     ############################################################################
     ## Update / Draw / Handle Events Methods                                  ##
     ############################################################################
-    def update(self, dt):
-        pass;
-
     def draw(self, surface):
         self.__current_sprite.draw(surface);
 
     def handle_events(self, event):
+        #Check which type of event and pass to handler.
         if(event.type == pygame.locals.MOUSEBUTTONDOWN):
             self.__onMouseButtonDown();
         elif(event.type == pygame.locals.MOUSEBUTTONUP):
@@ -94,18 +117,24 @@ class Button(BaseWidget):
         elif(event.type == pygame.locals.MOUSEMOTION):
             self.__onMouseMotion();
 
+    ############################################################################
+    ## Mouse Events                                                           ##
+    ############################################################################
     def __onMouseButtonDown(self):
+        #COWTODO: Comment.
         pos = pygame.mouse.get_pos();
+        #COWTODO: Remove output.
         print "on mouse down", pos, self.__current_sprite.get_bounding_box();
-        if(self.__current_sprite.get_bounding_box().collidepoint(pos)):
+        if(self.get_bounding_box().collidepoint(pos)):
             self.__set_pressed_state();
 
     def __onMouseButtonUp(self):
+        #COWTODO: Comment.
         self.reset();
-    def __onMouseMotion(self):
-        pass;
+        if(self.__click_callback is not None):
+            self.__click_callback();
 
-    def __set_pressed_state(self):
-        self.__current_sprite = self.__pressed_sprite;
-    def __set_normal_state(self):
-        self.__current_sprite = self.__normal_sprite;
+    def __onMouseMotion(self):
+        #COWTODO: Comment.
+        #COWTODO: Implement.
+        pass;

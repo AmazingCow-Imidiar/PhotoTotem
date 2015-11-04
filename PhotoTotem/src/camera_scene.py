bc77d0caa1ba6ea1755b9a7f90baab51e3be7844
N2OMatt <n2o.matt@gmail.com>
2015-11-04 17:44:41 -0200

    Make the countdown sprites hidden by default.

--- PhotoTotem/src/camera_scene.py
+++ PhotoTotem/src/camera_scene.py
@@ -210,9 +210,7 @@ class CameraScene(BaseScene):
         self.__countdown_sprite.set_position(info["position"]);
         self.__countdown_sprite.load_image(info["sprites"][0]);
 
-        #Add to scene.
-        self.add(self.__countdown_sprite,
-                 layer = CameraScene.__LAYER_INDEX_COUNTDOWN_SPRITE);
+        #Countdown is hidden by default.
 
 
     ############################################################################
